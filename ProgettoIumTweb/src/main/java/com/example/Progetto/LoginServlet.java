eeeebadd1d741a3aa7bb4edd235cbb878965b103
MMat27 <bimbospecial46@gmail.com>
2023-08-19 21:23:38 +0200

    doGet mezza aggiustata

--- ProgettoIumTweb/src/main/java/com/example/Progetto/LoginServlet.java
+++ ProgettoIumTweb/src/main/java/com/example/Progetto/LoginServlet.java
@@ -22,7 +22,7 @@ public class LoginServlet extends HttpServlet {
         model = (Model) ctx.getAttribute("DAO");
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
         String username = request.getParameter("login");
         String password = request.getParameter("passwd");
@@ -35,31 +35,10 @@ public class LoginServlet extends HttpServlet {
                 if(utente.getAmministratore()) {
                     s.setAttribute("ruolo", "amministratore");
 
-                } else
+                } else {
                     s.setAttribute("ruolo", "utente");
-                if (s.getAttribute("ruolo").equals("amministratore")){
-                    // Effettua l'inserimento dei dati del docente
-                    //!!!NON è COMPLETO!!!
-                    //DOESN'T WORK--------------------------------------------------------------------------
-                    String nomeDocente = request.getParameter("name");
-                    String cognomeDocente = request.getParameter("surname");
-                    model.insertDocente(nomeDocente,cognomeDocente);
-                    try (PrintWriter out = response.getWriter()) {
-                        out.println("<!DOCTYPE html>");
-                        out.println("<html>");
-                        out.println("<head>");
-                        out.println("<title>Feedback positivo</title>");
-                        out.println("</head>");
-                        out.println("<body>");
-                        out.println("<p>Ciao " + s.getAttribute("username") + "</p>");
-                        out.println("<p>Hai ruolo " + s.getAttribute("ruolo") + "</p>");
-                        out.println("<p>Operazione completata con successo.</p>");
-                        out.println("</body>");
-                        out.println("</html>");
-                        return;
-                    }
-                    //----------------------------------------------------------------------------------
                 }
+                    //----------------------------------------------------------------------------------
                 try (PrintWriter out = response.getWriter()) {
                     out.println("Welcome back");
                 }
@@ -70,24 +49,7 @@ public class LoginServlet extends HttpServlet {
                 //s.setAttribute("ruolo", "fail");
             }
         }
-        try (PrintWriter out = response.getWriter()) {
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet GestisciSessione</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<p>Ciao ");
-            out.println(s.getAttribute("username") + "</p>");
-            out.println("<p>hai ruolo " + s.getAttribute("ruolo") + "</p>");
-            out.println("<p>non essendo un amministratore l'operazione è fallita</p>");
-            out.println("<p>Ricarica la pagina: ");
-            out.println("<form action=\"login-servlet\" method=\"post\">"
-                    + "   <input type=\"submit\" name=\"submit\" value=\"OK\"/>"
-                    + "</form> </p>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+
     }
 
     public void destroy() {
